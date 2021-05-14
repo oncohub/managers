d61375c7596d5258a31a8fa1b83e0cacc365fe0b
oncol <kwata@white-tails.com>
2021-05-14 21:14:12 +0900

    test

--- service-worker.js
+++ service-worker.js
@@ -56,6 +56,7 @@ self.addEventListener('install', function (e) {
         caches.open('managers-cache').then(cache => {
             // If the request for any of these resources fails, _none_ of the resources will be
             // added to the cache.
+            console.log(cache);
             return cache.addAll(targetUrl);
         })
     );
